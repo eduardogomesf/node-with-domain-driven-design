dcd3cb8ef0b851cff6f10b08128f39072ece60b3
Eduardo Gomes <eduardogoomesf@gmail.com>
2023-05-31 00:09:45 -0300

    feat: create entities

---
+++ src/entity/customer.ts
@@ -0,0 +1,46 @@
+import { Address } from "./address";
+
+export class Customer {
+
+    _id: string = "";
+    _name: string = ""
+    _address!: Address
+    _active: boolean = false
+
+    constructor(id: string, name: string) {
+        this._id = id
+        this._name = name
+        this.validate()
+    }
+
+    validate () {
+        if (this._name.length === 0) {
+            throw new Error("Name is required")
+        }
+
+        if (this._id.length === 0) {
+            throw new Error('Id is required')
+        }
+    }
+
+    changeName (name: string) {
+        this._name = name
+        this.validate()
+    }
+
+    activate () {
+        if (!!this._address) {
+            throw new Error("Address is required")
+        }
+
+        this._active = true
+    }
+
+    deactivate () {
+        this._active = false
+    }
+
+    set Address (address: Address) {
+        this._address = address
+    }
+}
