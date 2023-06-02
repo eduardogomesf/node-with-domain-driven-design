2f2cd0b66f3ecab4965f544d8894807666d4d637
Eduardo Gomes <eduardogoomesf@gmail.com>
2023-06-01 23:16:31 -0300

    test: add test cases for reward points implementation

--- test/service/order.service.spec.ts
+++ test/service/order.service.spec.ts
@@ -1,5 +1,6 @@
 import { OrderItem } from '../../src/entity/order_item'
 import { Order } from '../../src/entity/order'
+import { Customer } from '../../src/entity/customer'
 import { OrderService } from '../../src/service/order.service'
 
 describe('Order service unit tests', () => {
@@ -14,6 +15,16 @@ describe('Order service unit tests', () => {
         const total = OrderService.total([order1, order2])
 
         expect(total).toBe(500)
+    })
+
+    it('should place an order', () => {
+        const customer = new Customer('c1', 'Customer 1',)
+        const item1 = new OrderItem('oi1', 'Item 1', 10, 'p1', 1)
+
+        const order = OrderService.placeOrder(customer, [item1])
+
+        expect(customer.rewardPoints).toBe(5)
+        expect(order.total()).toBe(10)
     })
 
 })
