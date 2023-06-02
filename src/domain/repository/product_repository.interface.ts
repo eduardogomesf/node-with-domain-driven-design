60eb564ac7a1fa71c71f70ed44b712aa0848a50d
Eduardo Gomes <eduardogoomesf@gmail.com>
2023-06-02 20:51:29 -0300

    feat: add product model for sequelize orm

---
+++ src/domain/repository/product_repository.interface.ts
@@ -0,0 +1,6 @@
+import { Product } from "../entity/product";
+import { RepositoryInterface } from "./repository_interface";
+
+export interface ProductRepositoryInterface extends RepositoryInterface<Product> {
+
+}
